392b9880a2dec6d7c9666f4a1f7343ab136e8a3b
ILYA KOKHAN <ilyakokhan@gmail.com>
2022-03-18 09:29:44 +0300

    Change scan progress color

--- app/src/main/kotlin/tech/hombre/bluetoothchatter/ui/widget/ExpiringProgressBar.kt
+++ app/src/main/kotlin/tech/hombre/bluetoothchatter/ui/widget/ExpiringProgressBar.kt
@@ -37,7 +37,7 @@ class ExpiringProgressBar : View {
     }
 
     private val strokePaint: Paint = Paint().apply {
-        color = ContextCompat.getColor(context, R.color.colorScanProgress)
+        color = ContextCompat.getColor(context, R.color.colorAccent)
         strokeWidth = strokeThickness
         isDither = true
         style = Paint.Style.STROKE
